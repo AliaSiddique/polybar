bd35a53c851204057074c6337a64c119cbeb5a4a
Alia Siddique <aliaisreal@gmail.com>
2020-01-08 13:15:50 +0200

    lazy

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-## Add to your wm startup file.
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the process have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
